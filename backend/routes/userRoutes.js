8dd630b1d1b7c005e82b697995d522ab799f0b1a
samuellea <samuel.lea@live.co.uk>
2025-04-22 16:56:12 +0100

    Added Home page, beginning to move form submission logic to here. Creating 'sidebar' with New Users and Manage Users options'

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const {
+  fetchUsers,
   createUser,
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
 const router = express.Router();
 
+router.get('/', fetchUsers);
 router.post('/', createUser);
 router.patch('/:id', updateUser);
 router.delete('/:id', deleteUser);
